4e1d33de31e8c6864500fd2ffda33d270b67fc34
fahadshah868 <fahadshah868@gmail.com>
2019-07-03 18:56:29 +0500

    updations

---
+++ MMAPlus/Test Suites/MMA Plus Merchandising/MMA Plus Merchandising With Category Level Overwrite Scenarios.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>MMA Plus Merchandising With Category Level Overwrite Scenarios</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>4</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b9d1b4e0-a9dd-4488-91b6-f455213ee42b</testSuiteGuid>
+   <testCaseLink>
+      <guid>be68e7bb-0f96-4c0d-a684-97c2c6adc2e0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LaunchingApp/LaunchApp</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>8644ebf3-0b87-4389-b6e5-796ac7ab46da</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e9031150-d94d-4317-a6d8-65ad3f48be1c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Attendence/VisitAttendance_ForMerchandising</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ad5a054f-d6a1-4134-84eb-f5d775f6a0b2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/DistributionPoint/VisitDistributionPoint</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8fea2548-849c-451c-8df3-986197cdd96d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/SliderOptions/VisitSliderOptions</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d24dbc07-ac24-469b-8853-7038b4bc31af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/MerchandisingShopScenarios/VisitShopsWithCategoryLevelOverwritingScenarios</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>49381d56-201f-4d41-b1fe-d4bef0fe285a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MerchandisingShopScenarios/VisitShopsWithCategoryLevelOverwriteScenarios</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
